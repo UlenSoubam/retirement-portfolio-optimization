912e43b69b718b9a7723220d4d83c1aad220a3d6
Ulen <Ulensoubam55@gmail.com>
2025-03-25 15:35:14 +0530

    Update daily_return.py

--- codes/daily_return.py
+++ codes/daily_return.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 📌 Compute daily return for STOCKS
-stock_df = pd.read_csv(r"C:\Users\ulens\git_hub\tangency_portfolio\data\cleaned_synthetic_price_data.csv", parse_dates=["date"])
+stock_df = pd.read_csv("cleaned_synthetic_price_data.csv", parse_dates=["date"])
 
 # Sort by symbol and date
 stock_df = stock_df.sort_values(by=["symbol", "date"])
@@ -12,10 +12,10 @@ stock_df["daily_return"] = stock_df.groupby("symbol")["close"].pct_change()
 # Save to CSV
 stock_df[["date", "symbol", "daily_return"]].dropna().to_csv("stock_daily_return.csv", index=False)
 
-print("✅ Stock daily return data saved as 'stock_daily_return.csv'")
+print(" Stock daily return data saved as 'stock_daily_return.csv'")
 
 
-# 📌 Compute daily return for INDEX
+# Compute daily return for INDEX
 index_df = pd.read_csv(r"C:\Users\ulens\git_hub\tangency_portfolio\data\synthetic_nifty50_ohlc.csv", parse_dates=["date"])
 
 # Sort by date
@@ -27,4 +27,4 @@ index_df["daily_return"] = index_df["close"].pct_change()
 # Save to CSV
 index_df[["date", "daily_return"]].dropna().to_csv("index_daily_return.csv", index=False)
 
-print("✅ Index daily return data saved as 'index_daily_return.csv'")
+print("Index daily return data saved as 'index_daily_return.csv'")
