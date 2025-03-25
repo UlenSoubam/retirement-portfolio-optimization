1df5d5407bc4f77ac6137fa0901a4a8b50eebd6c
Ulen <Ulensoubam55@gmail.com>
2025-03-25 15:50:10 +0530

    Update tangency_portfolio.py

--- codes/tangency_portfolio.py
+++ codes/tangency_portfolio.py
@@ -3,7 +3,7 @@ import numpy as np
 import ast  # Safer evaluation
 
 # Load Monte Carlo results
-portfolio_df = pd.read_csv(r"C:\Users\ulens\git_hub\tangency_portfolio\code\data cleaning\clean_monte_carlo_simulation.csv")
+portfolio_df = pd.read_csv("clean_monte_carlo_simulation.csv")
 
 # Identify the portfolio with the highest Sharpe Ratio
 tangency_portfolio = portfolio_df.loc[portfolio_df["Sharpe Ratio"].idxmax()]
@@ -20,7 +20,7 @@ symbols = expected_annual_return.index.tolist()  # Get stock names
 tangency_df = pd.DataFrame({"Stock": symbols, "Weight": weights_list})
 
 # Save the cleaned Tangency Portfolio with column names
-tangency_df.to_csv(r"C:\Users\ulens\git_hub\tangency_portfolio\tangency_portfolio.csv", index=False)
+tangency_df.to_csv("tangency_portfolio.csv", index=False)
 
 print("Tangency Portfolio Identified! Results saved to tangency_portfolio.csv.")
 print("Optimal Portfolio Weights:\n", tangency_df)
